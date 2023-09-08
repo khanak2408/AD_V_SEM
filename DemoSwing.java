05b1781a113d77a063132b33532dc51a9decdcd6
praveen yadav <praveenyadavskit@gmail.com>
2023-09-08 14:19:05 +0530

    First Swing Program

---
+++ DemoSwing.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+import java.awt.*;
+class RegistrationForm extends JFrame{
+    Container c;
+    JLabel lblFirstName,lblLastName;
+    JTextField txtFirstName,txtLastName;
+    JButton btnRegister;
+
+    public RegistrationForm(){
+        c=getContentPane();
+        lblFirstName=new JLabel();
+        lblFirstName.setText("First Name");
+        lblLastName=new JLabel("Last Name");
+        txtFirstName=new JTextField(10);
+        txtLastName=new JTextField(10);
+
+        btnRegister=new JButton("Register User");
+
+        JLabel lblCourse = new JLabel("choose your Course");
+        JCheckBox cb1=new JCheckBox("C++");
+        JCheckBox cb2=new JCheckBox("Java");
+        JCheckBox cb3=new JCheckBox("Python");
+
+        JLabel lblGender=new JLabel("Select your gender");
+        JRadioButton rdo1=new JRadioButton();
+        rdo1.setText("Male");
+        JRadioButton rdo2=new JRadioButton();
+        rdo2.setText("Female");
+
+
+        ButtonGroup bg=new ButtonGroup();
+        bg.add(rdo1);
+        bg.add(rdo2);
+
+        lblFirstName.setBounds(20, 20, 100, 30);
+        txtFirstName.setBounds(130, 20, 150, 30);
+        lblLastName.setBounds(20, 60, 100, 30);
+        txtLastName.setBounds(130, 60, 150, 30);
+        
+        lblCourse.setBounds(20, 100, 150, 30);
+        cb1.setBounds(20, 130, 100, 30);
+        cb2.setBounds(130, 130, 100, 30);
+        cb3.setBounds(240, 130, 100, 30);
+                
+        lblGender.setBounds(20, 170, 150, 30);
+        rdo1.setBounds(20, 200, 100, 30);
+        rdo2.setBounds(130, 200, 100, 30);
+                
+        btnRegister.setBounds(20, 240, 100, 30);
+
+        c.add(lblFirstName);
+        c.add(txtFirstName);
+        c.add(lblLastName);
+        c.add(txtLastName);
+        c.add(lblCourse);
+        c.add(cb1);
+        c.add(cb2);
+        c.add(cb3);
+        c.add(lblGender);
+        c.add(rdo1);
+        c.add(rdo2);
+        c.add(btnRegister);
+    }
+}
+public class DemoSwing{
+    public static void main(String args[]){
+        RegistrationForm f=new RegistrationForm();
+        f.setTitle("First Swing Form");
+        f.setLayout(null);
+        f.setSize(400,400);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
